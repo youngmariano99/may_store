a0aca24e9746de04dd70779780c284bef020eb65
Mariano.Y <youngmariano99@gmail.com>
2025-06-04 17:33:15 -0300

    clase para mostrar productos

--- may_store/urls.py
+++ may_store/urls.py
@@ -18,11 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 from products.views import ProductListView
-
+from django.urls import include
 urlpatterns = [
     path('', ProductListView.as_view() , name='index'),
     path('usuarios/login', views.login_view, name='login'),
     path('usuarios/logout', views.logout_view, name='logout'),
     path('usuarios/registro', views.register, name='register'),
     path('admin/', admin.site.urls),
+    path('productos/', include('products.urls'))
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 # Create your views here.
 from .models import Product
 
@@ -12,5 +13,18 @@ class ProductListView(ListView):
         context = super().get_context_data(**kwargs)
         context['message'] ='Listado de productos'
         context['products'] = context ['product_list']
+        
+        return context
+    
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'products/product.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        print(context)
         
         return context
